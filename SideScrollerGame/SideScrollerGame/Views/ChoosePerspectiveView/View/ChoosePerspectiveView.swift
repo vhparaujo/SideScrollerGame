e58738af0bf9bdfc672c070cf7addb54c2bfafd6
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-10-07 14:12:47 -0300

    fixing bugs

--- SideScrollerGame/SideScrollerGame/Views/ChoosePerspectiveView/View/ChoosePerspectiveView.swift
+++ SideScrollerGame/SideScrollerGame/Views/ChoosePerspectiveView/View/ChoosePerspectiveView.swift
@@ -32,20 +32,13 @@ struct ChoosePerspectiveView: View {
             if (mpManager.gameStartInfo.localPlayerStartInfo.eraSelection != mpManager.gameStartInfo.otherPlayerStartInfo.eraSelection) && (mpManager.gameStartInfo.localPlayerStartInfo.eraSelection != nil && mpManager.gameStartInfo.otherPlayerStartInfo.eraSelection != nil) {
                 
                 Button {
-                    if (mpManager.gameStartInfo.localPlayerStartInfo.isStartPressed == .yes){
-                        
                         playerStartInfo.isStartPressed = .yes
-                    } else{
-                        playerStartInfo.isStartPressed = .no
-                    }
+                    
                     mpManager.sendInfoToOtherPlayers(content: playerStartInfo)
                     
                 } label: {
-                    if (mpManager.gameStartInfo.localPlayerStartInfo.isStartPressed == .yes) {
-                        Text("Ready")
-                    } else {
-                        Text("Cancel")
-                    }
+                    Text("Ready")
+                  
                 }
             }
         }
