9148c38b24dc1f2ef21886c13330c6edbebe5866
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-10-07 14:14:36 -0300

    fixing bugs

--- SideScrollerGame/SideScrollerGame/Views/Content/View/ContentView.swift
+++ SideScrollerGame/SideScrollerGame/Views/Content/View/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @Bindable var managerMP = MultiplayerManager()
     var body: some View {
-        if managerMP.gameStartInfo.localPlayerStartInfo.isStartPressed == .yes && managerMP.gameStartInfo.localPlayerStartInfo.isStartPressed == .yes {
+        if managerMP.gameStartInfo.localPlayerStartInfo.isStartPressed == .yes && managerMP.gameStartInfo.otherPlayerStartInfo.isStartPressed == .yes {
             
             if let era = managerMP.gameStartInfo.localPlayerStartInfo.eraSelection {
         GameView(currentSceneType: .first(era), mpManager: managerMP)
