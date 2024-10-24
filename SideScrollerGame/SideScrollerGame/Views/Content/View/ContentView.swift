d2dfdd98a1c71dea549dff45d4ed2666618d1875
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-10-24 15:12:24 -0300

    apagando codigo inutil

--- SideScrollerGame/SideScrollerGame/Views/Content/View/ContentView.swift
+++ SideScrollerGame/SideScrollerGame/Views/Content/View/ContentView.swift
@@ -12,9 +12,8 @@ struct ContentView: View {
     
     var body: some View {
         if mpManager.gameStartInfo.localPlayerStartInfo.isStartPressed == .yes && mpManager.gameStartInfo.otherPlayerStartInfo.isStartPressed == .yes {
-            if let era = mpManager.gameStartInfo.localPlayerStartInfo.eraSelection {
         GameView()
-            }
+            
             
         }else if mpManager.choosingEra {
             ChoosePerspectiveView(playerStartInfo: .init( isStartPressed: .no))
