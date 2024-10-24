fc96fd060f3d5734a5096d7fbbaf0d4c66290607
vhparaujo <vhugopacheco59@gmail.com>
2024-10-24 17:46:01 -0300

    criaçao da cena 2, somente o tileset

--- SideScrollerGame/SideScrollerGame/Assets/Textures/PlayerTextureState.swift
+++ SideScrollerGame/SideScrollerGame/Assets/Textures/PlayerTextureState.swift
@@ -6,8 +6,6 @@
 //
 import SpriteKit
 
-
-
 enum PlayerTextureState: Codable {
     case running
     case idle
@@ -18,58 +16,57 @@ enum PlayerTextureState: Codable {
     
     func textures(for era: PlayerEra) -> [SKTexture] {
         switch (self, era) {
-            case (.running, .present):
-                return SKSpriteNode.loadTextures(prefix: "player-run-present", count: 6)
-            case (.running, .future):
-                return SKSpriteNode.loadTextures(prefix: "player-run-future", count: 6)
-
-            case (.idle, .present):
-                return SKSpriteNode.loadTextures(prefix: "player-idle-present", count: 4)
-            case (.idle, .future):
-                return SKSpriteNode.loadTextures(prefix: "player-idle-future", count: 4)
-                
-            case (.jumping, .present):
-                return SKSpriteNode.loadTextures(prefix: "player-jump-present", count: 2)
-            case (.jumping, .future):
-                return SKSpriteNode.loadTextures(prefix: "player-jump-future", count: 2)
-                
-            case (.climbing, .present):
-                return SKSpriteNode.loadTextures(prefix: "player-climb-present", count: 3)
-            case (.climbing, .future):
-                return SKSpriteNode.loadTextures(prefix: "player-climb-future", count: 3)
-                
-            case (.grabbing, .present):
-                return SKSpriteNode.loadTextures(prefix: "player-grab-present", count: 2)
-            case (.grabbing, .future):
-                return SKSpriteNode.loadTextures(prefix: "player-grab-future", count: 2)
+        case (.running, .present):
+            return SKSpriteNode.loadTextures(prefix: "player-run-present", count: 6)
+        case (.running, .future):
+            return SKSpriteNode.loadTextures(prefix: "player-run-future", count: 6)
             
-            case (.hurt, .present):
-                return SKSpriteNode.loadTextures(prefix: "player-hurt-present", count: 2)
-            case (.hurt, .future):
-                return SKSpriteNode.loadTextures(prefix: "player-hurt-future", count: 2)
+        case (.idle, .present):
+            return SKSpriteNode.loadTextures(prefix: "player-idle-present", count: 4)
+        case (.idle, .future):
+            return SKSpriteNode.loadTextures(prefix: "player-idle-future", count: 4)
+            
+        case (.jumping, .present):
+            return SKSpriteNode.loadTextures(prefix: "player-jump-present", count: 2)
+        case (.jumping, .future):
+            return SKSpriteNode.loadTextures(prefix: "player-jump-future", count: 2)
+            
+        case (.climbing, .present):
+            return SKSpriteNode.loadTextures(prefix: "player-climb-present", count: 3)
+        case (.climbing, .future):
+            return SKSpriteNode.loadTextures(prefix: "player-climb-future", count: 3)
+            
+        case (.grabbing, .present):
+            return SKSpriteNode.loadTextures(prefix: "player-grab-present", count: 2)
+        case (.grabbing, .future):
+            return SKSpriteNode.loadTextures(prefix: "player-grab-future", count: 2)
+            
+        case (.hurt, .present):
+            return SKSpriteNode.loadTextures(prefix: "player-hurt-present", count: 2)
+        case (.hurt, .future):
+            return SKSpriteNode.loadTextures(prefix: "player-hurt-future", count: 2)
         }
     }
     
     var timePerFrame: TimeInterval {
         switch self {
-            case .running:
-                return 0.1
-            case .idle:
-                return 0.2
-            case .jumping:
-                return 0.15
-            case .climbing:
-                return 0.2
-            case .grabbing:
-                return 0.1
-            case .hurt:
+        case .running:
+            return 0.1
+        case .idle:
+            return 0.2
+        case .jumping:
+            return 0.15
+        case .climbing:
+            return 0.2
+        case .grabbing:
+            return 0.1
+        case .hurt:
             return 0.1
         }
     }
 }
 
 extension SKSpriteNode {
-    
     static func loadTextures(prefix: String, count: Int) -> [SKTexture] {
         var textures: [SKTexture] = []
         for i in 1...count {
