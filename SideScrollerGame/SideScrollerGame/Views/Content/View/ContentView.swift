a263603896d5a626097f45cc478d7d2d5a93f06e
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-10-29 14:55:11 -0300

    fixing bugs

--- SideScrollerGame/SideScrollerGame/Views/Content/View/ContentView.swift
+++ SideScrollerGame/SideScrollerGame/Views/Content/View/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
 //        GameView(currentSceneType: .first(.future))
         
         if !mpManager.gameFinished {
-            GameView(currentSceneType: .first(.future))
+            GameView()
          } else {
              EndGameView()
          }
