97d0a2ecb3fbaaa01731e29d511f103011d962c7
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-10-30 14:52:07 -0300

    reorganizando as cenas

--- SideScrollerGame/SideScrollerGame/Views/Content/View/ContentView.swift
+++ SideScrollerGame/SideScrollerGame/Views/Content/View/ContentView.swift
@@ -11,11 +11,15 @@ struct ContentView: View {
     @State var mpManager = MultiplayerManager.shared
     
     var body: some View {
-        if mpManager.gameStartInfo.local.isStartPressed == .yes && mpManager.gameStartInfo.other.isStartPressed == .yes && !mpManager.gameFinished{
+        if mpManager.gameStartInfo.local.isStartPressed == .yes && mpManager.gameStartInfo.other.isStartPressed == .yes &&
+            !mpManager.gameFinished{
         GameView()
             
             
-        }else if mpManager.gameFinished {
+        }else if mpManager.gameFinished &&
+                    !mpManager.choosingEra &&
+                    mpManager.gameStartInfo.local.isStartPressed == .yes &&
+                    mpManager.gameStartInfo.other.isStartPressed == .yes {
             EndGameView()
             
         }else if mpManager.choosingEra {
