e7ef420aa338c39385bbeebb765acecd3839d2ea
Eduardo R Vieira <eduardovieirayt@gmail.com>
2024-10-30 00:43:00 -0300

    fix: security headers to responses.

--- SideScrollerGame/SideScrollerGame/Views/Content/View/ContentView.swift
+++ SideScrollerGame/SideScrollerGame/Views/Content/View/ContentView.swift
@@ -11,18 +11,18 @@ struct ContentView: View {
     @State var mpManager = MultiplayerManager.shared
     
     var body: some View {
-        if mpManager.gameStartInfo.localPlayerStartInfo.isStartPressed == .yes && mpManager.gameStartInfo.otherPlayerStartInfo.isStartPressed == .yes {
-            if mpManager.gameStartInfo.localPlayerStartInfo.eraSelection != nil {
-        GameView()
-            }
-            
-        }else if mpManager.choosingEra {
-            ChoosePerspectiveView(playerStartInfo: .init( isStartPressed: .no))
-        }else{
-            JoinGameView()
-        }
-
+//        if mpManager.gameStartInfo.localPlayerStartInfo.isStartPressed == .yes && mpManager.gameStartInfo.otherPlayerStartInfo.isStartPressed == .yes {
+//            if mpManager.gameStartInfo.localPlayerStartInfo.eraSelection != nil {
 //        GameView()
+//            }
+//            
+//        }else if mpManager.choosingEra {
+//            ChoosePerspectiveView(playerStartInfo: .init( isStartPressed: .no))
+//        }else{
+//            JoinGameView()
+//        }
+
+        GameView()
 
     }
 }
