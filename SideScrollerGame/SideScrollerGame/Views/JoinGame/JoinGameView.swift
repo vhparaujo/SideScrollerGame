61352b63c4f1040cf0a4d35a36320b35544e22e6
Eduardo R Vieira <eduardovieirayt@gmail.com>
2024-11-04 16:45:00 -0300

    a

--- SideScrollerGame/SideScrollerGame/Views/JoinGame/JoinGameView.swift
+++ SideScrollerGame/SideScrollerGame/Views/JoinGame/JoinGameView.swift
@@ -10,18 +10,44 @@ import GameKit
 
 struct JoinGameView: View {
     @Bindable var managerMP: MultiplayerManager = .shared
+    
     var body: some View {
-        Button("Start Game"){
-            if managerMP.automatch {
-                // Turn automatch off.
-                GKMatchmaker.shared().cancel()
-                managerMP.automatch = false
+        GeometryReader { geometry in
+            VStack() {
+                // Title Image
+                HStack {
+                    Image("titleImage")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geometry.size.width * 0.5)
+                    Spacer()
+                }.padding(.top, 50)
+                Spacer()
+                Spacer()
+                
+                HStack {
+                    Image("playButtonImage")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geometry.size.width * 0.2)
+                        .onTapGesture {
+                            if managerMP.automatch {
+                                // Turn automatch off.
+                                GKMatchmaker.shared().cancel()
+                                managerMP.automatch = false
+                            }
+                            managerMP.choosePlayer()
+                        }
+                    Spacer()
+                }
+                .padding(.horizontal, 20)
+                Spacer()
             }
-            managerMP.choosePlayer()
-        }.buttonStyle(.borderedProminent)
+            .padding(.horizontal, 40)
+
+        }
     }
 }
-
 #Preview {
     JoinGameView(managerMP: MultiplayerManager())
 }
