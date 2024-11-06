a30abace6df68ef6b5a66d9527e04e6f7600782f
Eduardo R Vieira <eduardovieirayt@gmail.com>
2024-11-06 15:39:35 -0300

    a

--- SideScrollerGame/SideScrollerGame/Views/ChoosePerspectiveView/View/ChoosePerspectiveView.swift
+++ SideScrollerGame/SideScrollerGame/Views/ChoosePerspectiveView/View/ChoosePerspectiveView.swift
@@ -16,19 +16,48 @@ struct ChoosePerspectiveView: View {
     
     var body: some View {
         VStack{
+            
+            Image("ChoosePerspective")
+                .padding(.top)
+                .padding()
+            
             HStack {
                 ForEach(PlayerEra.allCases, id: \.self) { perspective in
-                    Button("\(perspective)") {
-                        self.playerStartInfo.eraSelection = perspective
-                        mpManager.sendInfoToOtherPlayers(content: playerStartInfo)
+                    
+                    if perspective == PlayerEra.future {
+                        ZStack {
+                            Image("FutureChoose")
+                                .scaleEffect(0.7)
+                            VStack {
+                                Image("player-future-idle-left-1")
+                                    .scaledToFit()
+                                    .scaleEffect(0.35)
+                            }
+                            .offset(y: -50)
+                        }
+                        .onTapGesture {
+                            self.playerStartInfo.eraSelection = perspective
+                            mpManager.sendInfoToOtherPlayers(content: playerStartInfo)
+                        }
+                    }else {
+                        ZStack {
+                            Image("PresentChoose")
+                                .scaleEffect(0.7)
+                            VStack {
+                                Image("player-present-idle-right-1")
+                                    .scaledToFit()
+                                    .scaleEffect(0.32)
+                            }
+                            .offset(y: -38)
+                        }
+                        .onTapGesture {
+                            self.playerStartInfo.eraSelection = perspective
+                            mpManager.sendInfoToOtherPlayers(content: playerStartInfo)
+                        }
                     }
-                    .padding()
-                    .background(self.mpManager.gameStartInfo.other.eraSelection == perspective ? Color.red : self.playerStartInfo.eraSelection == perspective ? Color.blue : Color.gray)
-                    .foregroundColor(.white)
-                    .cornerRadius(8)
                 }
             }
-        
+            
             if (mpManager.gameStartInfo.local.eraSelection != mpManager.gameStartInfo.other.eraSelection) && (mpManager.gameStartInfo.local.eraSelection != nil && mpManager.gameStartInfo.other.eraSelection != nil) {
                 
                 Button {
